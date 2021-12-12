5edcc27c6effcbbdac1a02f39add01c34dfc6ba0
Oleksandr <81925784+NOSOV-ksa@users.noreply.github.com>
2021-12-12 23:41:12 +0200

    hbs...

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import './sass/main.scss';
+import photoCard from './partials/templait-gallary.hbs'
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import fetchAPI from './partials/fetch.js';
+// import creatList from './partials/templait-gallary.js'
+// import SimpleLightbox from "simplelightbox";
 
+const refs = {
+    searchQuery: document.querySelector('[name="searchQuery"]'),
+    btnSubmit: document.querySelector('[type="submit"]'),
+    cardItems: document.querySelector('.card-item'),
+    search: "",
+}
 
-const inputName = document.querySelector('[name="searchQuery"]');
-const btnSearch = document.querySelector('[type="submit"]');
+// console.log(refs.search)
 
-let textSearch = "";
+refs.searchQuery.addEventListener('input', (e) => {
+    refs.search = e.currentTarget.value;
+});
 
+refs.btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    fetchAPI(refs.search)
+        .then(request => request.json())
+        .then(cards => renderCard(cards));
+});
 
-inputName.addEventListener('input', onInputName);
-
-// console.log(textSearch)
-
-function onInputName(event) {
-    const textSearch = event.target.value.trim();
-
-    btnSearch.addEventListener('click', onSearchImg);
-    function onSearchImg(event) {
-        event.preventDefault();
-        fetchAPI(textSearch);
-    }
-
-};
-
+function renderCard(cards) {
+    console.log(cards)
+    const markup = photoCard(cards.hits)
+    refs.cardItems.innerHTML = markup
+}
--- src/partials/fetch.js
+++ src/partials/fetch.js
@@ -1,17 +1,30 @@
-const axios = require('axios').default;
+const axios = require('axios');
 
 const keyAPI = "24739954-0d3e858e9f263100a20e7db59";
-const URL_DOMAIN = "https://pixabay.com/api/";
+const baseURL = "pixabay.com";
+const perPage = 40;
 
-axios({
-  method: 'post',
-  url: '/user/12345',
-  data: {
-    firstName: 'Fred',
-    lastName: 'Flintstone'
-  }
-});
+// axios({
+  //   method: 'get',
+  //   url: 'baseURL',
+  //   data: {
+    //     firstName: 'Fred',
+    //     lastName: 'Flintstone'
+    //   }
+    // });
 
-function fetchImg(keyWord) {
-    return fetch()
+    // function fetchImg(keyWord) {
+
+// }
+
+//   .then(response => console.log(response))
+
+
+export default function  onSearch(searchTypeObj) {
+
+  return fetch(`https://${baseURL}/api/?key=${keyAPI}&q=${searchTypeObj}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=${perPage}`)
+
+
 }
+// onSearch("cat", 30)
+
